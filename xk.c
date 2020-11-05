9fa310cb45b50691e5158185d3e082347dd47b5e
Jeff Frasca <thatguy@jeff-frasca.name>
2020-11-05 13:07:48 -0800

    First pass refactor of xk.c -- break out ls and warp, add column title line

--- xk.c
+++ xk.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <wayland-client.h>
 #include <wayland-client-protocol.h>
@@ -42,10 +43,16 @@ handle_view_info(void *data, struct xuake_control *xuake_control, int32_t view_i
     int32_t workspace, int32_t type, int32_t fullscreen, int32_t output,
     int32_t x, int32_t y, int32_t width, int32_t height, const char *title)
 {
+    char buf[64];
+
     if (view_id == 0 && type == 0)
         exit(0);
 
-    printf("%d %d %d [%d,%d+%d+%d] %s\n", view_id, workspace, type, x, y, width, height, title);
+    memset(buf, 0, 64*sizeof(char));
+
+    snprintf(buf, 64, "[%d,%d+%d+%d]", x, y, width, height);
+
+    printf("%4d  %2d  %s  %-24s%s\n", view_id, workspace, type ? "X" : "W", buf, title);
 }
 
 static struct xuake_control_listener xc_listener = {
@@ -100,10 +107,61 @@ usage(void)
     );
 }
 
+void
+run_ls(char **argv, int argc)
+{
+    int ws = -1;
+
+    if (argc == 2) {
+        ws = strtol(argv[1], NULL, 10);
+        if (errno) {
+            usage();
+            return;
+        }
+    } else if (argc > 2) {
+        usage();
+        return;
+    }
+    xuake_control_list_views(xc, ws);
+    printf("VIEW  WS  T  %-24sTITLE\n", " GEOMETRY");
+}
+
+void
+run_warp(char **argv, int argc)
+{
+    char *s, *S[4];
+    int i, x, y, w = 0, h = 0;
+    int view_id = strtol(argv[1], NULL, 10);
+    if (errno || !view_id) return;
+
+    S[0] = argv[2];
+    for (s = argv[2], i = 1; *s; s++) {
+        switch (*s) {
+        case ',': case '+':
+            *s = 0;
+            S[i++] = ++s;
+            break;
+        }
+    }
+    x = strtol(S[0], NULL, 10);
+    if (errno) return;
+    y = strtol(S[1], NULL, 10);
+    if (errno) return;
+    if (i >= 3) {
+        w = strtol(S[2], NULL, 10);
+        if (errno) return;
+        h = strtol(S[3], NULL, 10);
+        if (errno) return;
+    }
+
+    xuake_control_warp_view(xc, view_id, x, y, w, h);
+}
+
 int
 main(int argc, char **argv)
 {
     int i;
+    bool forever = false;
 
     display = wl_display_connect(NULL);
 
@@ -128,68 +186,36 @@ main(int argc, char **argv)
         if (!strcmp("about", argv[1])) {
             xuake_control_about(xc);
 
-            while (wl_display_dispatch(display) != -1);
+            forever = true;
         } else if (!strcmp("exit", argv[1])) {
             xuake_control_exit(xc);
-            wl_display_dispatch(display);
         } else if (!strcmp("free", argv[1])) {
             xuake_control_set_monacle(xc, 0);
-            wl_display_dispatch(display);
         } else if (!strcmp("mono", argv[1])) {
             xuake_control_set_monacle(xc, 1);
-            wl_display_dispatch(display);
-        } else if (!strcmp("ls", argv[1]) && argc == 2) {
-            xuake_control_list_views(xc, -1);
-            while (wl_display_dispatch(display) != -1);
-        } else if (!strcmp("ls", argv[1]) && argc == 3) {
-            int ws = strtol(argv[2], NULL, 10);
-            xuake_control_list_views(xc, ws);
-            while (wl_display_dispatch(display) != -1);
+        } else if (!strcmp("ls", argv[1])) {
+            run_ls(&argv[1], argc - 1);
+            forever = true;
         } else if (!strcmp("ws", argv[1]) && argc == 3) {
             int ws = strtol(argv[2], NULL, 10);
-            xuake_control_workspace(xc, ws);
-            wl_display_dispatch(display);
+            if (!errno)
+                xuake_control_workspace(xc, ws);
         } else if (!strcmp("focus", argv[1]) && argc == 3) {
             int view_id = strtol(argv[2], NULL, 10);
-            xuake_control_focus_view(xc, view_id);
-            wl_display_dispatch(display);
+            if (!errno && view_id)
+                xuake_control_focus_view(xc, view_id);
         } else if (!strcmp("impulse", argv[1]) && argc > 2) {
             uint32_t slot = strtol(argv[2], NULL, 10);
             if (!errno && slot <= XK_MAX_IMPULSE) {
                 xuake_control_impulse(xc, slot);
-                wl_display_dispatch(display);
             }
         } else if (!strcmp("warp", argv[1]) && argc > 3) {
-            char *s, *S[4];
-            int i, x, y, w = 0, h = 0;
-            int view_id = strtol(argv[2], NULL, 10);
-            if (errno || !view_id)
-                exit(1);
-
-            S[0] = argv[3];
-            for (s = argv[3], i = 1; *s; s++) {
-                switch (*s) {
-                case ',': case '+':
-                    *s = 0;
-                    S[i++] = ++s;
-                    break;
-                }
-            }
-            x = strtol(S[0], NULL, 10);
-            y = strtol(S[1], NULL, 10);
-            if (i >= 3) {
-                w = strtol(S[2], NULL, 10);
-                h = strtol(S[3], NULL, 10);
-            }
-
-            xuake_control_warp_view(xc, view_id, x, y, w, h);
-            wl_display_dispatch(display);
+            run_warp(&argv[1], argc - 1);
         } else if (!strcmp("close", argv[1]) && argc > 2) {
             int view_id = strtol(argv[2], NULL, 10);
             if (errno || !view_id)
                 exit(1);
             xuake_control_close_view(xc, view_id);
-            wl_display_dispatch(display);
         } else if (!strcmp("version", argv[1])) {
             printf("xk version %s\n", XUAKE_VERSION);
         } else if (!strcmp("help", argv[1])) {
@@ -200,6 +226,8 @@ main(int argc, char **argv)
     } else {
         usage();
     }
+
+    while (wl_display_dispatch(display) != -1 && forever);
 
     wl_display_disconnect(display);
 
